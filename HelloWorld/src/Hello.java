555be69ff866633bb645ee342b2bb9ff3bfaacb7
Ariel Nykanen <arielnykan3n@gmail.com>
2019-10-20 16:39:07 +0300

    uploading first Hello World app

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        int myFirstVar = 5;
+        int mySecondNum = 12;
+        int myThirdNum = 6;
+
+        System.out.println("Hello World ");
+
+        System.out.println("My number is " + myFirstVar);
+    }
+}
