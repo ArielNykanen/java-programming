5cad2abf2a5b82cbc257fec1a20a432a1c8ddd2a
Ariel Nykanen <arielnykan3n@gmail.com>
2019-10-21 12:32:35 +0300

    adding char and boolean progress

---
+++ charAndBoolean/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // char can only store one character.
+        char myChar = 'D';
+        System.out.println(myChar);
+
+        // storing character in unicode
+        char myUnicodeChar = '\u0044';
+        System.out.println(myUnicodeChar);
+
+        //Boolean can store true false;
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+    }
+}
