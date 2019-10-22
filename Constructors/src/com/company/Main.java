9a0d8f4038c72419e7fd92489b329eda3bd6835f
Ariel Nykanen <arielnykan3n@gmail.com>
2019-10-22 21:37:30 +0300

    adding constructors Challenge

---
+++ Constructors/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+       Account bobsAccount = new Account();
+        bobsAccount.withDrawal(100.0);
+        bobsAccount.deposite(50.0);
+        bobsAccount.withDrawal(100.0);
+        bobsAccount.deposite(51.0);
+        bobsAccount.withDrawal(100.0);
+
+    }
+}
